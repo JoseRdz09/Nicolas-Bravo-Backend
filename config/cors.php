314d271b0df3482b7bd4d96831a6d51efbafbb8c
JoseRdz09 <163470294+JoseRdz09@users.noreply.github.com>
2024-10-17 08:57:51 -0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,13 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://nicolas-bravo-front-kyrl0pg2a-joserdz09s-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization'],
 
     'exposed_headers' => [],
 
